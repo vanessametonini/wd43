becf8770dfefc922cdccb46b679d8bf3bcb2c600
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-05 18:59:35 -0200

    Exercícios: include

--- sobre.php
+++ sobre.php
@@ -8,23 +8,7 @@
   <link rel="stylesheet" href="css/sobre.css">
 </head>
 <body>
-    <header class="container">
-        <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
-
-        <p class="sacola">
-        Nenhum item na sacola de compras
-        </p>
-
-        <nav class="menu-opcoes">
-            <ul>
-            <li><a href="#">Sua Conta</a></li>
-            <li><a href="#">Lista de Desejos</a></li>
-            <li><a href="#">Cartão Fidelidade</a></li>
-            <li><a href="sobre.html">Sobre</a></li>
-            <li><a href="#">Ajuda</a></li>
-        </ul>
-        </nav>
-    </header>
+    <?php include("cabecalho.php"); ?>
     <article class="container">
         <section>
             <h1 class="titulo">A Mirror Fashion</h1>
@@ -86,15 +70,6 @@
             </ul>
         </section>
     </article>
-    <footer>
-        <div class="container">
-            <img src="img/logo-rodape.png" alt="Logo Mirror Fashion">
-            <ul class="social">
-            <li><a href="http://facebook.com/mirrorfashion">Facebook</a></li>
-            <li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
-            <li><a href="http://plus.google.com/mirrorfashion">Google+</a></li>
-            </ul>
-        </div>
-    </footer>
+    <?php include("rodape.php"); ?>
 </body>
 </html>
