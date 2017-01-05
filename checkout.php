24d99bb6e529acec7e821bdddc8618110ec2465f
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-05 20:22:15 -0200

    Exercícios: checkout da compra

---
+++ checkout.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Checkout Mirror Fashion</title>
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+
+    <h1>Ótima escolha!</h1>
+    <p>Obrigado por comprar na Mirror Fashion!
+    Preencha seus dados para efetivar a compra.</p>
+
+    <h2>Sua compra</h2>
+    <dl>
+        <dt>Produto</dt>
+        <dd><?= $_POST['nome'] ?></dd>
+
+        <dt>Preço</dt>
+        <dd><?= $_POST['preco'] ?></dd>
+        
+        <dt>Cor</dt>
+        <dd><?= $_POST['cor'] ?></dd>
+
+        <dt>Tamanho</dt>
+        <dd><?= $_POST['tamanho'] ?></dd>
+    </dl>
+
+</body>
+</html>
--- produto.php
+++ produto.php
@@ -7,7 +7,7 @@
         <h1>Fuzzy Cardigan</h1>
         <p>por apenas R$ 129,00</p>
 
-        <form>
+        <form action="checkout.php" method="POST">
             <fieldset class="cores">
                 <legend>Escolha a cor:</legend>
 
@@ -31,6 +31,9 @@
                 <legend>Escolha o tamanho:</legend>
                 <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
             </fieldset>
+
+            <input type="hidden" name="nome" value="Fuzzy Cardigan">
+            <input type="hidden" name="preco" value="129.00">
 
             <input type="submit" class="comprar" value="Comprar">
         </form>
