fd0cadaf0836f21723c43851ebf2c58238936a11
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-05 18:52:24 -0200

    Exercícios: executando o PHP

--- sobre.php
+++ sobre.php
@@ -29,7 +29,7 @@
         <section>
             <h1 class="titulo">A Mirror Fashion</h1>
             <p>
-                A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo. Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de participação em 118 deles.
+                A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo. Fundada há <?php print date("Y") - 1932; ?> anos, possui filiais em 124 países, sendo líder de mercado com mais de 90% de participação em 118 deles.
             </p>
             <p>
                 Nosso centro de distribuição fica em <a href="https://maps.google.com.br/?q=Jacarezinho">Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que distribuem nossos produtos às casas do mundo todo. Nosso centro de distribuição:
