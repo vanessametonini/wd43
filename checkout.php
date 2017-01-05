6d1ace5eea139ec4a6dd221fe5fc374d66a806a1
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-05 20:56:02 -0200

    Exercícios: página de checkout

--- checkout.php
+++ checkout.php
@@ -3,28 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <title>Checkout Mirror Fashion</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <meta name="viewport" content="width=device-width">
 </head>
 <body>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Ótima escolha!</h1>
+            <p>Obrigado por comprar na Mirror Fashion!
+            Preencha seus dados para efetivar a compra.</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2>Sua compra</h2>
+            </div>
+            <div class="panel-body">
+                <img src="img/produtos/foto2-<?= $_POST["cor"] ?>.png" class="img-thumbnail img-responsive">
+                <dl>
+                    <dt>Produto</dt>
+                    <dd><?= $_POST['nome'] ?></dd>
 
-    <h1>Ótima escolha!</h1>
-    <p>Obrigado por comprar na Mirror Fashion!
-    Preencha seus dados para efetivar a compra.</p>
+                    <dt>Preço</dt>
+                    <dd><?= $_POST['preco'] ?></dd>
 
-    <h2>Sua compra</h2>
-    <dl>
-        <dt>Produto</dt>
-        <dd><?= $_POST['nome'] ?></dd>
+                    <dt>Cor</dt>
+                    <dd><?= $_POST['cor'] ?></dd>
 
-        <dt>Preço</dt>
-        <dd><?= $_POST['preco'] ?></dd>
-        
-        <dt>Cor</dt>
-        <dd><?= $_POST['cor'] ?></dd>
-
-        <dt>Tamanho</dt>
-        <dd><?= $_POST['tamanho'] ?></dd>
-    </dl>
-
+                    <dt>Tamanho</dt>
+                    <dd><?= $_POST['tamanho'] ?></dd>
+                </dl>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
